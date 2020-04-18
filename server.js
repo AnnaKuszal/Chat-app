2fc8a3c0ad6de059391ca7b98c8bcc75eeb16021
Anna Kuszal <anna.kuszal@o2.pl>
2020-04-18 23:29:40 +0200

    Integrate socket.io to server

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const path = require('path');
+const socket = require('socket.io');
 
 const app = express();
+
+
 
 const messages = [];
 
@@ -11,6 +14,7 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname + '/client/index.html'));
 });
 
-app.listen(8000, () => {
+const server = app.listen(8000, () => {
   console.log('Server is running on port: 8000');
 });
+const io = socket(server);
